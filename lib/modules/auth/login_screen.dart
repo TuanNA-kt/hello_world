4d4b75461e2df8779207d7c7bc7bfb58f5a2a629
TuanNA <nguyentuan1104k3@gmail.com>
2025-11-24 02:33:59 +0700

    Minor changes

--- lib/modules/auth/login_screen.dart
+++ lib/modules/auth/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:hello_world/common/app_colors.dart';
 import 'package:hello_world/common/widgets/custom_text_field.dart';
+import 'package:hello_world/common/widgets/primary_button.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
@@ -12,26 +14,49 @@ class LoginScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const SizedBox(height: 80),
+              const SizedBox(height: 70),
               SvgPicture.asset('assets/images/ic_mail_paper_plane.svg'),
-              Text('Trai nghiem Awesome chat', style: TextStyle(fontSize: 26)),
-              Text('Dang nhap', style: TextStyle(fontSize: 32)),
+              Text('Trải nghiệm Awesome chat', style: TextStyle(fontSize: 26)),
+              Text(
+                'Đăng nhập',
+                style: TextStyle(
+                  fontSize: 32,
+                  color: AppColors.primaryBlue,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
               SizedBox(height: 30),
-              CustomTextField(key: UniqueKey(),
+              CustomTextField(
+                key: UniqueKey(),
                 hint: 'yourname@gmail.com',
                 iconPath: 'assets/images/facebook.svg',
                 label: 'EMAIL',
-                type: TextFieldType.mail),
+                type: TextFieldType.mail,
+              ),
               SizedBox(height: 20),
-              CustomTextField(key: UniqueKey(),
-                  hint: '*******',
-                  iconPath: 'assets/images/facebook.svg',
-                  label: 'MAT KHAU',
-                  type: TextFieldType.password)
+              CustomTextField(
+                key: UniqueKey(),
+                hint: '*******',
+                iconPath: 'assets/images/facebook.svg',
+                label: 'MAT KHAU',
+                type: TextFieldType.password,
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  'Quên mật khẩu?',
+                  style: TextStyle(fontSize: 14, color: AppColors.primaryBlue),
+                ),
+              ),
+              PrimaryButton(text: 'Đăng ký', onPressed: onPressed)
             ],
           ),
         ),
       ),
     );
   }
+
+  void onPressed() {
+  }
+
 }
